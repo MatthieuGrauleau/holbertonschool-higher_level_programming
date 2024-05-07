72caff6c1ee5ae97f667f99a77a4a86376798e3d
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-07 15:15:29 +0200

    fix pycodestyle

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(s):
     for c in s:
-        print("{:c}".format(ord(c) - 32 if 97 <= ord(c) <= 122 else ord(c)), end="")
+        print("{:c}".format(ord(c) - 32
+                            if 97 <= ord(c) <= 122 else ord(c)), end="")
     print()
-    
