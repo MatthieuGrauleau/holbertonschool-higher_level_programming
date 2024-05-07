0c096e9d3aacf9f8667b25e10cc22b79902c35f6
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-07 18:25:39 +0200

    modifing spaces

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    num_args = len(sys.argv) - 1
+    from sys import argv
+    num_args = len(argv) - 1
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
-        print("1 argument :")
+        print("1 argument:")
     else:
-        print("{} arguments :".format(num_args))
+        print("{} arguments:".format(num_args))
 
     for i in range(num_args):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, argv[i + 1]))
