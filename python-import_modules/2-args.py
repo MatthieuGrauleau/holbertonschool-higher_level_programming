dc79fbacd14f093ea126aee3f8b2034fe5edce71
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-07 18:21:46 +0200

    adding a s

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     num_args = len(sys.argv) - 1
     if num_args == 0:
-        print("0 argument.")
+        print("0 arguments.")
     elif num_args == 1:
         print("1 argument :")
     else:
