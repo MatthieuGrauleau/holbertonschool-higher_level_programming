3d9fa76199cde81ff26bb0bb9b173ed788024614
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-08 10:39:17 +0200

    Write a program that prints all the names defined by the compiled module hidden_4.pyc (please download it locally in your sandbox using curl).

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    module_names = dir(hidden_4)
-    for name in module_names:
+    for name in dir(hidden_4):
         if name[:2] != "__":
             print(name)
