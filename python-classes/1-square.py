45d3c446af96981e5eeb47ed6912093b7c2a1b14
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-17 09:42:01 +0200

    class Square that defines a square by: (based on 0-square.py)

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write an empty class """
+
+
+class Square:
+    """Define a square."""
+    def __init__(self, size):
+        """Initialize a new Square.
+
+		Args:
+			size (int): size of a square
+		"""
+        self.__size = size
