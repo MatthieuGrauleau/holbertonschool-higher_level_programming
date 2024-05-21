84820edc29372bb3d3da65c9c8b36df515fa2e37
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-21 12:11:35 +0200

    unittest

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -20,3 +20,7 @@ class TestMaxInteger(unittest.TestCase):
     
     def test_negative_and_positive(self):
         self.assertEqual(max_integer([-1, 2, -3, 4]), 4)
+    
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
