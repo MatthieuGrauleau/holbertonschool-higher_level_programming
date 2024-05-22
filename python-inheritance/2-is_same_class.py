b029d585acf58d2ff3ed0091724eb241d623a5cf
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-22 15:33:55 +0200

    adding do test

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """returns True if the object is"""
+
+
 def is_same_class(obj, a_class):
     """returns True if the object is
     exactly an instance of the specified class ; otherwise False."""
