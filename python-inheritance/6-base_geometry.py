6ffc60d05895622d9d4da87bf2f7d3880fd0577f
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-22 16:12:46 +0200

    adding a medoth that raises an exeception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """aclass BaseGeometry"""
+    def area(self):
+        """raises an Exception with the message
+        area() is not implemented"""
+        raise Exception("area() is not implemented")
