112a91ab97eeff686ac6c166f8ea59a098c3af5f
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-22 10:58:29 +0200

    that prints the list, but sorted (ascending sort)

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-def print_sorted(self):
+"""mylist that inherits from list"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """that prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
