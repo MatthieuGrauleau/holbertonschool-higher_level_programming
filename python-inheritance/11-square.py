a8439b1ebfcd036aabac909e6bdc0db1313115d2
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-23 13:29:54 +0200

    adding str

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class Square that inherits from Rectangle (9-rectangle.py):"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a class square"""
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
