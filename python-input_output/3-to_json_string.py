994b84de0801d0b937659f17abbdbb150ec2b731
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-27 12:10:32 +0200

    adding docstring

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -4,5 +4,10 @@ representation of an object (string):"""
 import json
 
 
+import json
+
 def to_json_string(my_obj):
+    """Convert a Python object to a JSON string representation.
+    Args:my_obj: The Python object to be converted.
+    Returns:A JSON string representation of the input object."""
     return json.dumps(my_obj)
