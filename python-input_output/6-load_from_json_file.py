feeca1143270ae627c589c6a4e6932de016a7d88
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-28 10:27:41 +0200

    script that adds all arguments to a Python list, and then save them to a file:

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Load data from a JSON file and return the deserialized object."""
+import json
+
+
+def load_from_json_file(filename):
+    """Load data from a JSON file and return the deserialized object.
+    Args:filename (str): The name of the JSON file to load.
+    Returns:The deserialized object from the JSON file."""
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
