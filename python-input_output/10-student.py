e4d45983728976c884f25bb33da6de067f6047b7
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-29 09:38:38 +0200

    creation of json with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""class Student that defines a student"""
+
+
+class Student:
+    """
+    Represents a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+                attrs (list): List of attributes to include in the dictionary.
+                                          If None, all attributes are included.
+
+        Returns:
+                dict: Dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {
+                key: value for key,
+                value in self.__dict__.items() if key in attrs}
