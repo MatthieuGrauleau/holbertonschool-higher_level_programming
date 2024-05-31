dbb76fca3ac2c501a447a4b8bb45d30b25d3a8bd
MatthieuGrauleau <m.grauleau@gmail.com>
2024-05-31 16:35:45 +0200

    Consuming and processing data from an API using Python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,11 +4,49 @@ import requests
 
 
 def fetch_and_print_posts():
-    get = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print(f"Status Code: {requests.status_code}")
-    
-    if requests.status_code == 200:
-        parse = requests.json()
-        for post in parse:
+    """
+    Fetches posts from a RESTful API and prints their titles.
+
+    This function sends a GET request to the specified API
+    endpoint and retrieves a list of posts.
+    It then prints the title of each post.
+
+    Args:
+            None
+
+    Returns:
+            None
+    """
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
             print(post['title'])
-    
+
+
+def fetch_and_save_posts():
+    """
+    Fetches posts from the given API endpoint and saves them to a CSV file.
+
+    This function sends a GET request to the API endpoint
+    "https://jsonplaceholder.typicode.com/posts"
+    to retrieve a list of posts. It then extracts the necessary
+    information from each post and saves
+    it to a CSV file named "posts.csv" with the columns 'id',
+    'title', and 'body'.
+
+    Returns:
+            None
+    """
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if response.status_code == 200:
+        posts = response.json()
+        post_list = [{'id': post['id'], 'title': post['title'],
+                      'body': post['body']} for post in posts]
+        csv_file = 'posts.csv'
+        with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(post_list)
