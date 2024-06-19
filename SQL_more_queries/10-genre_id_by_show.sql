63251505f3a378782e28d0392acae7afa8d142db
MatthieuGrauleau <m.grauleau@gmail.com>
2024-06-19 15:42:31 +0200

    script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,6 @@
 --  script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_show.title, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres
-ON tv_show.id = tv_show_genres.tv_show_id
-ORDER BY tv_show.title ASC,
-tv_show_genres.genre_id ASC;
+JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
