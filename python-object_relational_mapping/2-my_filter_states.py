a71e785e0ee878293446af67ea7ce416c6ddd922
MatthieuGrauleau <m.grauleau@gmail.com>
2024-06-20 17:38:42 +0200

    adding BINARY

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -31,9 +31,10 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    select = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+    query = """SELECT * FROM states WHERE
+    BINARY name = '{}' ORDER BY id ASC""".format(
         state_name)
-    cursor.execute(select)
+    cursor.execute(query)
     states = cursor.fetchall()
 
     for state in states:
