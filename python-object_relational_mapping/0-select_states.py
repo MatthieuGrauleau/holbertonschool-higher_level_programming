49fb5b0f34123747e81b9dfeaa691739c3e4a9b7
MatthieuGrauleau <m.grauleau@gmail.com>
2024-06-20 10:58:42 +0200

    script that lists all states from the database hbtn_0e_0_usa:

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "_main_":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+	)
+    
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id")
+    rows = cursor.fetchall()
+    
+    for row in rows:
+        print(row)
+    
+    cursor.close()
+    db.close()
